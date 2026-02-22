283503fdac3d9eac4f401da0a500be7fd8659f59
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2026-02-22 17:35:11 -0300

    feat: adicionar tipos Category e Entry

---
+++ src/types/category.ts
@@ -0,0 +1,8 @@
+export type Category = {
+  _id: string;
+  title: string;
+  color: string;
+  expense: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+};
---
+++ src/types/entry.ts
@@ -0,0 +1,11 @@
+export type Entry = {
+  _id: string;
+  title: string;
+  value: number;
+  date: string;
+  details: string;
+  category: string;
+  user?: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
